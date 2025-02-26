787a949dee21df358766933feed10c119a00ed50
daveny <davenyen@gmail.com>
2025-02-26 19:44:53 +0100

    Maze object
    Signed-off-by: daveny <davenyen@gmail.com>

--- maze.py
+++ maze.py
@@ -20,18 +20,22 @@ class Maze:
         self.cell_size_y = cell_size_y
         self.win = win
         self._cells = []
-    #     self._create_cells()
+        self._create_cells()
     
-    # def _create_cells(self):
-    #     lst = []
-    #     for i in range(self.num_rows):
-    #         for j in range(self.num_cols):
-    #             x1 = self.x1 + i * self.cell_size_x
-    #             x2 = x1 + self.cell_size_x
-    #             y1 = self.y1 + j * self.cell_size_y
-    #             y2 = y1 + self.cell_size_y
-    #             lst.append(Cell(x1, x2, y1, y2, True, True, True, True, self.win))
-    #     self._cells = lst
+    def _create_cells(self):
+        lst = []
+        for i in range(self.num_rows):
+             for j in range(self.num_cols):
+                 x1 = self.x1 + i * self.cell_size_x
+                 x2 = x1 + self.cell_size_x
+                 y1 = self.y1 + j * self.cell_size_y
+                 y2 = y1 + self.cell_size_y
+                 lst.append(Cell(x1, x2, y1, y2, True, True, True, True, self.win))
+        self._cells = lst
 
-        # for item in self._cells:
-        #     item.draw()
+        for item in self._cells:
+            item.draw()
+
+    def _animate(self):
+        self.win.redraw()
+        time.sleep(0.05)
